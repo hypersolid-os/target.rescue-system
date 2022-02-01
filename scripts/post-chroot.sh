4ee84f3d43db0eb54604765abcb175e9c1eb677d
Andi Dittrich <andi@andidittrich.de>
2022-02-01 14:05:00 +0100

    debian bullsye upgrade + systemd networkd

--- scripts/post-chroot.sh
+++ scripts/post-chroot.sh
@@ -10,3 +10,14 @@ systemctl enable getty@tty1.service
 
 # additional mountpoints
 mkdir /mnt/data
+
+# acpid
+systemctl enable acpid.service
+
+# systemd network management
+systemctl disable dhcpcd.service
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+
+# systemd managed resolve.conf
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
